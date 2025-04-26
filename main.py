0bfc2dcca8b7ac3c3d65bc04c790824796a16232
Ian-Collins1252 <ian-collins@ku.edu>
2025-04-26 15:42:04 -0500

    Initial scripts for characters

---
+++ main.py
@@ -0,0 +1,15 @@
+# Names: Del Endecott, Ian Collins, Zeidan Abdelrahman
+# KUIDs: , 3051520, 
+# Date: 4/25/25
+# Date last modified: 4/26/25
+# Course: EECS 510
+# Purpose: Run main simulation of Zelda character interactions
+
+# Zelda Dialog Accept String = {'exp'(('tell''exp')*+('feed''story')*)*('yes''quest'+'no')'bye'}
+# Impa Dialog Accept String = {'exp'('tell''exp')*('yes''quest'+'no')'bye'}
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
