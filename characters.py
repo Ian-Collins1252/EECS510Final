85252e8d7cc0e21f44299287173eba7dac7cf0ce
Ian-Collins1252 <ian-collins@ku.edu>
2025-05-02 14:15:49 -0500

    Adding Zelda class

--- characters.py
+++ characters.py
@@ -29,16 +29,70 @@ class Link():
         else:
             self.cur_state = transitions[(self.cur_state, input)]
 
+    # Returns a bool on if the converstation string is a valid given the current stack variable
     def dialog(self, item, string):
         #Consumes stack item and pushes
-        items = ['E', 'R', 'Q', 'F', 'B', '']
+        # [Exposistion, Responce, Quest, Feed, Bye, Story, Yes, No, 'Empty']
+        items = ['E', 'R', 'Q', 'F', 'B', 'S', 'Y', 'N', '']
         responce = {items[1]:(self.vocab[4], items[0]), #Asking for more info
                     items[1]:(self.vocab[2], items[2]), #Accepting quest
                     items[1]:(self.vocab[3], items[4]), #Declining quest
                     items[3]:(self.vocab[9], items[5]), #Give backstory after fed
                     items[4]:(self.vocab[10], items[5])} #Leave interaction
+        if string[0] == '':
+            return False
         
-        if item not in responce.keys:
-            return None
-        else:
-            return 
+        match item:
+            case 'R':
+                if string[0] not in {'Y', 'N', 'B'}:
+                    return False
+                else:
+                    return True
+            case 'F':
+                if string[0] not in {'S'}:
+                    return False
+                else:
+                    return True
+            case 'Q':
+                if string[0] not in {'B'}:
+                    return False
+                else:
+                    return True
+    
+    # TODO: Create interface for Turing Machine combat
+
+class Zelda():
+    def __init__(self):
+        state = ['Idle', 'Dialog']
+        vocab = ['idle', 'exposition', 'quest', 'feed', 'hi', 'bye']
+        cur_state = state[0]
+
+    # Returns a bool on if the converstation string is a valid given the current stack variable
+    def dialog(self, item, string):
+        #Consumes stack item and pushes
+        # [Exposistion, Responce, Quest, Feed, Bye, Story, Yes, No, 'Empty']
+        items = ['E', 'R', 'Q', 'F', 'B', 'S', 'Y', 'N', '']
+        responce = {items[1]:(self.vocab[4], items[0]), #Asking for more info
+                    items[1]:(self.vocab[2], items[2]), #Accepting quest
+                    items[1]:(self.vocab[3], items[4]), #Declining quest
+                    items[3]:(self.vocab[9], items[5]), #Give backstory after fed
+                    items[4]:(self.vocab[10], items[5])} #Leave interaction
+        if string[0] == '':
+            return False
+        
+        match item:
+            case 'E':
+                if string[0] not in {'Y', 'N', 'B'}:
+                    return False
+                else:
+                    return True
+            case 'B':
+                if string[0] not in {'S'}:
+                    return False
+                else:
+                    return True
+            case 'Q':
+                if string[0] not in {'B'}:
+                    return False
+                else:
+                    return True
